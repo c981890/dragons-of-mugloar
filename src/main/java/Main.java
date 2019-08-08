9671fcf44dc6f965c1b1f6f47159d309e49a8d46
Aiki Trumm <aiki.trumm@mail.ee>
2019-08-08 13:02:16 +0300

    Refactoring Main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,16 @@
 
-
-import Response.SolvedMessage;
-import Response.PurchasedItem;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         Logger logger = LoggerFactory.getLogger(Main.class);
-        Request request = new Request();
-        Gson gson = new Gson();
-        String gameStartUrl = "https://dragonsofmugloar.com/api/v2/game/start";
-        String responseForGameStart = request.POSTRequest(gameStartUrl);
-        Game game = gson.fromJson(responseForGameStart, Game.class);
+        Game game = getGame();
 
 
         while (game.getLives() > 0) {
@@ -37,5 +25,13 @@ public class Main {
 
         logger.info(game.toString());
 
+    }
+
+    private static Game getGame() throws IOException {
+        Request request = new Request();
+        Gson gson = new Gson();
+        String gameStartUrl = "https://dragonsofmugloar.com/api/v2/game/start";
+        String responseForGameStart = request.POSTRequest(gameStartUrl);
+        return gson.fromJson(responseForGameStart, Game.class);
     }
 }
